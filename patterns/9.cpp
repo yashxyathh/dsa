fb1a6168d5dfed3b87bf93203ac5bdf2aba60575
YASHASVI YATHARTH <yashasviyatharth07@gmail.com>
2026-07-06 21:51:45 +0530

    Add pattern generation program to display a diamond shape

---
+++ patterns/9.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+void pattern(int n){
+    for (int i=0;i<n;i++){
+        for(int j=0;j<n-1-i;j++){
+            cout<< " ";
+        }
+        for(int j=0;j<i*2+1;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<n-1-i;j++){
+            cout<< " ";
+        }
+        cout<<endl;
+    }
+    for (int i = 0; i <n; i++)
+    {
+        for (int j=0;j<i;j++){
+            cout<< " ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<< "*";
+        }
+        for (int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+        
+
+        cout<<endl;
+    
+}
+int main (){
+    int n=5;
+    // cin>>n;
+    pattern(n);
+    return 0;
+}
+
+
